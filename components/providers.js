cdecc29795c210f4f477608adbb3a9dd354e7a71
aniket <yash.d.habib1@gmail.com>
2023-06-27 16:31:24 +0530

    added the vercel url

--- components/providers.js
+++ components/providers.js
@@ -9,7 +9,7 @@ import {
 
 export const Providers = ({ children }) => {
   const client = new ApolloClient({
-    uri: "http://localhost:3000/api/graphql",
+    uri: "https://pomodoro-mwa85gwa2-yashhabib01.vercel.app/api/graphql",
     cache: new InMemoryCache(),
   });
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
